cfb90c8243c08936c3406612e6e6256ef3eb5554
Ilya Tzvirko <zaroxsino@gmail.com>
2021-06-23 19:22:36 +0300

    Create Nullarray

---
+++ lecture_5/lecture_5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lecture_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            getNullArray();
+        }
+        static void getNullArray()
+        {
+            object[] nullArray = new object[0];
+        }
+    }
+}
