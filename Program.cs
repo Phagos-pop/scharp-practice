c1e530fb83d6537fd6fcbb60b8c7e8d377aa9058
Ilya Tzvirko <zaroxsino@gmail.com>
2021-06-23 12:29:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
