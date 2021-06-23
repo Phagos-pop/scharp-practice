5d3a96311097c10554b8ada6508f63c691097b2c
Ilya Tzvirko <zaroxsino@gmail.com>
2021-06-23 19:36:32 +0300

    Second Task

--- lecture_5/lecture_5/Program.cs
+++ lecture_5/lecture_5/Program.cs
@@ -6,11 +6,35 @@ namespace lecture_5
     {
         static void Main(string[] args)
         {
-            getNullArray();
+            GetNullArray();
+            CreateArray();
         }
-        static void getNullArray()
+        static void GetNullArray()
         {
             object[] nullArray = new object[0];
+        }
+        static void CreateArray()
+        {
+            object[] array = new object[3];
+            array[0] = 32;
+            array[1] = 'A';
+            array[2] = "Hello";
+
+            foreach (int i in array)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+            int number =  Convert.ToInt32(array[0]) + 10 ;
+            array[0] = number;
+
+            string str = Convert.ToString(array[2]);
+            string finStr = str + " , guys!";
+
+            foreach (int i in array)
+            {
+                Console.WriteLine(array[i]);
+            }
         }
     }
 }
