0e9ca9db357f487b0d1ec680334a963fb947f3bf
Ilya Tzvirko <zaroxsino@gmail.com>
2021-07-26 19:31:39 +0300

    Task 1

---
+++ lecture_13/lecture_13/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lecture_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uniquetels uniquetels = new Uniquetels();
+            Uniquetels uniquetels1 = new Uniquetels();
+            Uniquetels uniquetels2 = new Uniquetels();
+            Uniquetels uniquetels3 = new Uniquetels();
+            Uniquetels uniquetels4 = new Uniquetels();
+            Uniquetels uniquetels5 = new Uniquetels();
+
+            Uniquetels.StartWith(12);
+
+            Uniquetels uniquetels6 = new Uniquetels();
+            Uniquetels uniquetels7 = new Uniquetels();
+            Uniquetels uniquetels8 = new Uniquetels();
+            Uniquetels uniquetels9 = new Uniquetels();
+            Uniquetels uniquetels10 = new Uniquetels();
+
+            Uniquetels.StartWith();
+
+            Uniquetels uniquetels11 = new Uniquetels();
+            Uniquetels uniquetels12 = new Uniquetels();
+            Uniquetels uniquetels13 = new Uniquetels();
+            Uniquetels uniquetels14 = new Uniquetels();
+        }
+    }
+}
---
+++ lecture_13/lecture_13/Uniquetels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lecture_13
+{
+    class Uniquetels
+    {
+        static int Id = default;
+
+        public Uniquetels()
+        {
+            Id++;
+            Console.WriteLine($"Число отсчета :{Id}");
+        }
+
+        public static void StartWith(int newNumber)
+        {
+            Id = newNumber;
+            Console.WriteLine($"Выбрано число отчета: {Id}");
+        }
+
+        public static void StartWith()
+        {
+            Console.WriteLine("Выберите число отсчета: ");
+            Id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Выбрано число отчета: {Id}");
+        }
+    }
+}
