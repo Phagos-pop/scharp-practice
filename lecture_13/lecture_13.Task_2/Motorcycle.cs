0c4f8186fee4164f8c8b1737c83c7c5714e2d7df
Ilya Tzvirko <zaroxsino@gmail.com>
2021-07-26 19:50:59 +0300

    Task 2

---
+++ lecture_13/lecture_13.Task_2/Motorcycle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+
+namespace lecture_13.Task_2
+{
+    class Motorcycle
+    {
+        public Guid VinNumber { get; set; }
+        public string Model;
+        public DateTime[] Trips { get; set; }
+        int _tripsCounter = default;
+
+        bool _isUpdateAvailable = true;
+
+        public Motorcycle()
+        {
+            //if (_isUpdateAvailable)
+            //    DownloadNewSoftware();
+
+            //VinNumber = Guid.Parse("f79564a4-5047-4799-8ea7-0bae9");
+        }
+
+        protected internal void Drive()
+        {
+            ++_tripsCounter;
+            Trips[_tripsCounter] = DateTime.Now;
+            Console.WriteLine("Let's Drive!");
+        }
+
+        internal void DownloadNewSoftware()
+        {
+            var getPackage = File.OpenRead("~/update.zip");
+            _isUpdateAvailable = false;
+        }
+
+        public override string ToString()
+        {
+            return Model ?? throw new NullReferenceException();
+        }
+    }
+}
---
+++ lecture_13/lecture_13.Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lecture_13.Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motorcycle motorcycle = new Motorcycle() { Trips = new DateTime[2] };
+            Console.WriteLine(motorcycle.ToString());
+
+            motorcycle.Drive();
+            motorcycle.Drive();
+            motorcycle.Drive();
+        }
+    }
+}
